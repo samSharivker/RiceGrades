9f228a1086d3cc3e9cdc089ab0f73fca1ed4a77c
Samuel Sharivker <114585707+samuels0052@users.noreply.github.com>
2023-10-03 19:16:40 -0400

    added favicon

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,21 +9,17 @@ function Nav() {
         });
     };
 
-    const isActiveLink = (pathname) => {
-        return window.location.pathname === pathname ? 'active' : '';
-    };
-
     return (
         <div class="container-fluid sticky-top" id="navbar-wrapper">
-            <nav class="navbar navbar-expand-sm">
+            <nav class="navbar navbar-expand-md">
                 <div class="container-fluid">
-                    <Link className={`navbar-brand ${isActiveLink('/')}`} to="/" onClick={scrollToTop}><span id="brand_span">RiceGrades</span></Link>
+                    <Link className="navbar-brand" to="/" onClick={scrollToTop}><span id="brand_span">RiceGrades</span></Link>
                     <button class="navbar-toggler navbar-toggler-icon" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     </button>
                     <div class="collapse navbar-collapse position-absolute bottom-0 end-0" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <Link className={`nav-link ${isActiveLink('/login')}`} to="/login" onClick={scrollToTop}><i class="bi bi-box-arrow-in-right"></i><span> Login</span></Link>
+                                <Link className="nav-link" to="/" onClick={scrollToTop}><i class="bi bi-box-arrow-in-right"></i><span> Login</span></Link>
                             </li>
                         </ul>
                     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,16 @@ function Home() {
     return (
         <div>
             <Nav />
+            <div id="home-1">
+                <div className="container-fluid">
+                    <p>Text</p>
+                </div>
+            </div>
+            <div id="home-2">
+                <div className="container-fluid">
+                    <p>Text</p>
+                </div>
+            </div>
         </div>
     )
 }
