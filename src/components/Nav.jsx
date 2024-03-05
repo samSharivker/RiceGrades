8f33022961a734d787ad1fa400cc1c410a945187
Samuel Sharivker <114585707+samuels0052@users.noreply.github.com>
2024-03-05 18:14:49 -0500

    updated homepage, got rid of bootstrap

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,31 +1,13 @@
-import { Link } from 'react-router-dom';
-
 function Nav() {
-
-    const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
-    };
-
     return (
-        <div class="container-fluid sticky-top" id="navbar-wrapper">
-            <nav class="navbar navbar-expand-md">
-                <div class="container-fluid">
-                    <Link className="navbar-brand" to="/" onClick={scrollToTop}><span id="brand_span">RiceGrades</span></Link>
-                    <button class="navbar-toggler navbar-toggler-icon" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    </button>
-                    <div class="collapse navbar-collapse position-absolute bottom-0 end-0" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <Link className="nav-link" to="/" onClick={scrollToTop}><i class="bi bi-box-arrow-in-right"></i><span> Login</span></Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
+        <nav>
+            <div class="logo">
+                <a href="#">Rice Grades</a>
+            </div>
+            <div class="links">
+                <a href="#">Login</a>
+            </div>
+        </nav>
     );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,41 @@
 import Nav from '../components/Nav';
 import Footer from '../components/Footer';
-import Logo from '../img/logo.svg';
+import Logo from '../img/logo.png';
 
 function Home() {
     return (
         <div>
             <Nav />
-            <div id="home-1">
-                <div className="container-fluid">
-                    <div className="container text-center">
-                        <div id="wrapper">
-                            <h id="home-top">Rice Grades</h> <br />
-                            <h id="home-mid">"Cook your Grades like your Rice"</h> <br />
-                            <h>- Jiaming Lin</h>
-                        </div>
+            <section id="header">
+                <div class="header">
+                    <div class="container">
+                        <p>"Cook your Grades like your Rice" - Jiaming Lin</p>
+                        <img src="https://placehold.co/400x400.png" alt="temp" />
                     </div>
                 </div>
-            </div>
-            <div id="home-2">
-                <div className="container-fluid">
-                    <div className="row pt-5">
-                        <div className="col-md-6 left">
-                            <div className="container text-center">
-                                <img src={Logo} alt="logo" class="img-fluid" height="300" width="300" />
+            </section>
+            <section id="about">
+                <div class="about">
+                    <div class="container">
+                        <img src={Logo} alt="temp" />
+                        <p>Professional Grading App for Students by Students</p>
+                    </div>
+                </div>
+            </section>
+            <div id="gallery">
+                <div class="gallery">
+                    <div class="container">
+                        <h>Gallery View</h> <br /><br /><br />
+                        <div class="slider-wrapper">
+                            <div class="slider">
+                                <img id="slide-1" src="https://placehold.co/640x360.png" alt="temp" />
+                                <img id="slide-2" src="https://placehold.co/640x360/000000/FFF" alt="temp" />
+                                <img id="slide-3" src="https://placehold.co/640x360/FF2400/FFF" alt="temp" />
                             </div>
-                        </div>
-                        <div className="col-md-6 right">
-                            <div className="container text-center">
-                                <h id="line-1">Professional Grading App</h> <br />
-                                <h id="line-2">Created by Students For Students</h>
+                            <div class="slider-nav">
+                                <a href="#slide-1"></a>
+                                <a href="#slide-2"></a>
+                                <a href="#slide-3"></a>
                             </div>
                         </div>
                     </div>
