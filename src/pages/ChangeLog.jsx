4b30deb0670f605289a54c76cfee89df203b3d35
Benjamin <benjaminthec@nycstudents.net>
2024-03-06 11:17:00 +0000

    merge

--- src/pages/ChangeLog.jsx
+++ src/pages/ChangeLog.jsx
@@ -1,7 +1,16 @@
+var whatChanged = "";
+const changing = false;
+if (changing === false){
+    whatChanged = "nothing changed";
+} else {
+    whatChanged = "Something changed";
+};
+
 function ChangeLog() {
     return (
         <div>
             <h1>ChangeLog</h1>
+            <p>{whatChanged}</p>
         </div>
     )
 }
--- src/pages/Developers.jsx
+++ src/pages/Developers.jsx
@@ -2,6 +2,11 @@ function Developers() {
     return (
         <div>
             <h1>Developers</h1>
+            <div id="nameDevs">
+                <p>Samuel Sharivker- </p>
+                <p>Benjamin Chau- </p>
+                <p>Zixuan Yu- </p>
+            </div>
         </div>
     )
 }
