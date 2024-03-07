c4cca76e4f054fdd3acb96df285af51ba00417b0
Samuel Sharivker <sjsharivker@hotmail.com>
2024-03-07 18:41:26 -0500

    added svg background

--- src/pages/Developers.jsx
+++ src/pages/Developers.jsx
@@ -17,6 +17,7 @@ export default function Developers() {
                         <p className="dev-bio">{samBio}</p>
                     </div>
                 </div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#79AC78" fill-opacity="1" d="M0,192L80,202.7C160,213,320,235,480,224C640,213,800,171,960,154.7C1120,139,1280,149,1360,154.7L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
             </section>
             <section>
                 <div className="developers">
@@ -27,6 +28,7 @@ export default function Developers() {
                         <p className="dev-bio">{yuBio}</p>
                     </div>
                 </div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#618264" fill-opacity="1" d="M0,192L80,202.7C160,213,320,235,480,224C640,213,800,171,960,154.7C1120,139,1280,149,1360,154.7L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
             </section>
             <section>
                 <div className="developers">
@@ -37,6 +39,7 @@ export default function Developers() {
                         <p className="dev-bio">{benBio}</p>
                     </div>
                 </div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#79AC78" fill-opacity="1" d="M0,192L80,202.7C160,213,320,235,480,224C640,213,800,171,960,154.7C1120,139,1280,149,1360,154.7L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
             </section>
             <Footer />
         </div>
