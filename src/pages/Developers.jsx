30354f00357e36ec01fb873456c69841152784e6
Samuel Sharivker <sjsharivker@hotmail.com>
2024-03-10 19:54:24 -0400

    updates

--- src/pages/Developers.jsx
+++ src/pages/Developers.jsx
@@ -12,7 +12,7 @@ export default function Developers() {
                 <div className="developers">
                     <div className="container" id="sam-dev-card">
                         <img src="https://github.com/samuels0052.png" height="150" width="150" alt="Developer" />
-                        <h>Samuel Sharivker</h>
+                        <a href="https://samuelsharivker.com"><h>Samuel Sharivker</h></a>
                         <p className="dev-title">Lead Developer</p>
                         <p className="dev-bio">{samBio}</p>
                     </div>
@@ -23,7 +23,7 @@ export default function Developers() {
                 <div className="developers">
                     <div className="container" id="yu-dev-card">
                         <img src="https://github.com/zixuany7791.png" height="150" width="150" alt="Developer" />
-                        <h>Zi Xuan Yu</h>
+                        <a href="https://github.com/zixuany7791"><h>Zi Xuan Yu</h></a>
                         <p className="dev-title">Chief Firebase Developer</p>
                         <p className="dev-bio">{yuBio}</p>
                     </div>
@@ -34,12 +34,11 @@ export default function Developers() {
                 <div className="developers">
                     <div className="container" id="ben-dev-card">
                         <img src="https://github.com/benjaminc8190.png" height="150" width="150" alt="Developer" />
-                        <h>Benjamin The Chau</h>
+                        <a href="https://github.com/benjaminc8190"><h>Benjamin The Chau</h></a>
                         <p className="dev-title">Chief React Developer</p>
                         <p className="dev-bio">{benBio}</p>
                     </div>
                 </div>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#79AC78" fill-opacity="1" d="M0,192L80,202.7C160,213,320,235,480,224C640,213,800,171,960,154.7C1120,139,1280,149,1360,154.7L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
             </section>
             <Footer />
         </div>
