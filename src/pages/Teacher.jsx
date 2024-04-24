9757b2dfc31734fe05d2522558149ed5aa9a3491
Samuel Sharivker <sjsharivker@hotmail.com>
2024-04-24 17:28:57 -0400

    added logout button to teacher page

--- src/pages/Teacher.jsx
+++ src/pages/Teacher.jsx
@@ -1,9 +1,25 @@
-export default function Teacher() {
-    var aymen = "dogwater"
-    console.log(aymen)
-    return (
+import React from 'react';
+import Nav from '../components/Nav';
+import Footer from '../components/Footer';
+import {
+  signOut
+} from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js'
+import { auth } from '../firebase'
+
+export default class Teacher extends React.Component {
+    render() {
+      const handleSignOut = () => {
+        signOut(auth)
+          .then(() => console.log("Sign Out"))
+          .catch((error) => console.log(error));
+      };
+      return (
         <div>
-            <h1>Please tell us that aymen is  {aymen} !!</h1>
+             <p>teacher page</p>
+            <button onClick={handleSignOut}>Sign Out</button>
+            <Footer />
         </div>
-    )
-}
+      );
+    }
+  };
+
