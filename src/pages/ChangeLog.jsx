ebf2678004d55145f480dec7da86605f665b9e1b
benjamin <benjaminc8190@gmail.com>
2024-05-14 17:16:17 -0400

    add comments to changelog code

--- src/pages/ChangeLog.jsx
+++ src/pages/ChangeLog.jsx
@@ -2,23 +2,47 @@ import Nav from '../components/Nav';
 import Footer from '../components/Footer';
 
 //Changes in May
-const may = ["Changelog is up to date(05/14/2024)", "Style update"]
-
+const may = [
+    "Changelog is up to date(05/14/2024)",
+    "Style update"
+];
+//Turns may array into list
 const listItemsMay = may.map((may) =>
     <li>{may}</li>
   );
 
 // Changes in April
-const april = ["Student Classroom view", "Button for deleting classroom", "Refresh tooltip button", "Editing grades feature", "Styling update", "error alerts with toasts", "error for fake accounts", "Adding students to classroom", "Feature for removing students", "Creating classrooms", "Add Firebase", "login form", "Teacher page", "Login errors alert", "Teacher page is protected"];
-
+const april = [
+    "Student Classroom view",
+    "Button for deleting classroom",
+    "Refresh tooltip button",
+    "Editing grades feature",
+    "Styling update",
+    "Error alerts with toasts",
+    "Error for fake accounts",
+    "Add students to classroom",
+    "Feature for removing students",
+    "Creating classrooms",
+    "Add Firebase",
+    "login form",
+    "Teacher page",
+    "Login errors alert",
+    "Teacher page is protected"
+];
+//Turns april array into list
 const listItemsApril = april.map((april) =>
     <li>{april}</li>
   );
 
 
 // Changes in March
-const march = ["Homepage", "Support form", "Profiles for developers", "Removed Bootstrap"];
-
+const march = [
+    "Homepage",
+    "Support form",
+    "Profiles for developers",
+    "Removed Bootstrap"
+];
+//Turns march array into list
 const listItemsMarch = march.map((march) =>
     <li>{march}</li>
   );
